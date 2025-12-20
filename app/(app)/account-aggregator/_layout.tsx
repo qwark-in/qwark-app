17bae50b069b3cc76525cbdfd540fa0e6d88c1cf
Harsh Mohite <harsh.mohite@qwark.in>
2025-12-20 19:48:13 +0530

    minor update

--- app/(app)/account-aggregator/_layout.tsx
+++ app/(app)/account-aggregator/_layout.tsx
@@ -6,6 +6,16 @@ export default function StackLayout() {
   return (
     <Stack screenOptions={{ ...screenOptions, headerRight: () => <QwarkLogo /> }}>
       <Stack.Screen name="select-banks" options={{ title: "Connect Accounts" }} />
+      <Stack.Screen
+        name="coming-soon"
+        options={{ title: "Connect Accounts", animation: "slide_from_bottom" }}
+      />
+      <Stack.Screen
+        name="create-session"
+        options={{ title: "Link Accounts", headerShown: false }}
+      />
+      <Stack.Screen name="account-discovery" options={{ title: "Connect Accounts" }} />
+      <Stack.Screen name="success" options={{ title: "Success", headerShown: false }} />
     </Stack>
   );
 }
