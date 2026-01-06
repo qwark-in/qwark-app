7283ccfdd3325c1cc9a8ba91d791311027e4f3f5
Harsh Mohite <harsh.mohite@qwark.in>
2026-01-06 17:48:05 +0530

    add proxy support for aa apis

--- data/api/aa/constants.ts
+++ data/api/aa/constants.ts
@@ -1,11 +1,16 @@
 // export const BASE_URL = 'https://qwarkuat.camsfinserv.com';
 
+import { Platform } from "react-native";
+
 const getBaseURL = (): string => {
-  const BASE_URL = process.env.EXPO_PUBLIC_DEV_AA_API_BASE_URL;
+  const BASE_URL =
+    Platform.OS !== "web"
+      ? process.env.EXPO_PUBLIC_DEV_AA_API_BASE_URL
+      : process.env.EXPO_PUBLIC_DEV_AA_API_WEB_BASE_URL;
   if (BASE_URL) {
     return BASE_URL;
   } else {
-    throw new Error('EXPO_PUBLIC_DEV_AA_API_BASE_URL not set in environment');
+    throw new Error("EXPO_PUBLIC_DEV_AA_API_BASE_URL not set in environment");
   }
 };
 
